f38f5111e3448b4be15e0d9012323752e6873d77
Yevhen Cherkes <ycherkes@outlook.com>
2024-02-11 16:57:34 +0100

    move response to file identically to request

--- test/HttpFileInTests/OpenLibraryTest.cs
+++ test/HttpFileInTests/OpenLibraryTest.cs
@@ -2,27 +2,24 @@ using HttpFileInTests.Extensions;
 using HttpFileInTests.Infrastructure;
 using Microsoft.DotNet.Interactive.HttpRequest;
 
-namespace HttpFileInTests
-{
-    public class OpenLibraryTest
-    {
-        [Theory]
-        [InlineData("OpenLibraryGetBook.http")]
-        public async Task GetBook_Success(string fileName)
-        {
-            // Arrange
-            var fileContent = await File.ReadAllTextAsync(Path.Combine("./TestData/", fileName));
-            
-            // Act
-            var result = await HttpFileUtility.RunHttpRequest(fileContent);
+namespace HttpFileInTests;
 
-            // Assert
-            result.EnsureSuccess();
+public class OpenLibraryTest
+{
+    [Theory]
+    [InlineData("OpenLibraryGetBook.http", "OpenLibraryGetBookResponseBody.json")]
+    public async Task GetBook_Success(string requestFileName, string responseFileName)
+    {
+        // Arrange
+        var requestFileContent = await File.ReadAllTextAsync(Path.Combine("./TestData/", requestFileName));
+        var expectedResponseContent = await File.ReadAllTextAsync(Path.Combine("./TestData/", responseFileName));
+
+        // Act
+        var commandResult = await HttpFileUtility.RunHttpRequest(requestFileContent);
+
+        // Assert
+        commandResult.EnsureSuccess();
             
-            var responseContent = (result.ReturnValueProduced?.Value as HttpResponse)?.Content?.Raw;
-            
-            Assert.Equal("""{"OL25158907M": {"bib_key": "OL25158907M", "info_url": "https://openlibrary.org/books/OL25158907M/Adventures_of_Sherlock_Holmes", "preview": "full", "preview_url": "https://archive.org/details/adventuresofsher00doyl1", "thumbnail_url": "https://covers.openlibrary.org/b/id/12082174-S.jpg"}}""", 
-                responseContent);
-        }
+        Assert.Equal(expectedResponseContent, (commandResult.ReturnValueProduced?.Value as HttpResponse)?.Content?.Raw);
     }
 }
